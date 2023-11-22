636812922ae38b76c743cd9f3078b18e40aca7ff
Waldir Júnior <waldirjunior.gpn@gmail.com>
2023-11-22 18:19:41 -0300

    Primeiro commit

---
+++ src/main/java/br/com/pagamentoFood/pagamentoFood/PagamentoFoodApplication.java
@@ -0,0 +1,13 @@
+package br.com.pagamentoFood.pagamentoFood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagamentoFoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagamentoFoodApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pagamentoFood/pagamentoFood/PagamentoFoodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pagamentoFood.pagamentoFood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagamentoFoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
